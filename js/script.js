f7269763e8bad68be8b730a039a701ef7f93ff00
SIELINOU NOUBISSIE ERIC ROMUALD <sielinou1605@gmail.com>
2023-11-17 19:30:24 +0100

    reponsive

--- js/script.js
+++ js/script.js
@@ -1,3 +1,13 @@
+/*==================    toggle icon navbar     =================*/
+
+let menuIcon = document.querySelector('#menu-icon');
+let navbar = document.querySelector('.navbar');
+
+menuIcon.onclick = () => {
+    menuIcon.classList.toggle('bx-x');
+    navbar.classList.toggle('active');
+}
+
 /*==================    scroll sections active link     =================*/
 
 let sections = document.querySelectorAll('section');
