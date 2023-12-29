8f6db219b379b7f703dd055ebbac1244fea06b41
SIELINOU NOUBISSIE ERIC ROMUALD <sielinou1605@gmail.com>
2023-12-29 16:47:02 +0100

    Cacher la modale GitHub 'indique'

--- js/UIs/modalFormUi.js
+++ js/UIs/modalFormUi.js
@@ -2,41 +2,44 @@ function schowModal(data) {
     const langue = localStorage.getItem('langue');
     console.log(langue);
     if(langue == 'fr') {
-        return `
-            <div id="maModal" class="modal" >
-                <div class="modal-container">
-                    <span class="lock" id="fermerModal">
-                        &times;
-                    </span>
-                    <div class="modal-container-title">
-                        <h4 >
-                           Remerciements pour prise de contact
-                        </h4>
-                    </div>
-                        <hr>
-                    <div class="modal-container-message">
-                        <p >
-                         &nbsp; &nbsp;Cher/Chère ${data.name}, </br>
-                         &nbsp; &nbsp;Je tenais à vous remercier chaleureusement pour avoir pris le temps de me contacter via mon
-                         portfolio. 
-                         Votre message ayant pour objet <b>"${data.subject}"</b> a été reçu avec gratitude.</br>
-                         &nbsp; &nbsp; je vous contacterais via: ${data.email}.</br>
-                         &nbsp; &nbsp; Votre contact est un privilège et je suis impatient de poursuivre nos échanges.</br></br>
-    
-                         &nbsp; &nbsp;
-                         Cordialement,</br>
-                         &nbsp; &nbsp;&nbsp; &nbsp; SIELINOU NOUBISSIE ERIC ROMUALD
-                         
-                        </p>
-                    </div>
-                    <div class="btn-modal">
-                        <div class="btn" id="ok-modal">
-                        ok
+        if(data.name !== '' && data.subject && data.email !=='') {}
+        if(data.name !== '' && data.subject && data.email !=='') {
+            return `
+                <div id="maModal" class="modal" >
+                    <div class="modal-container">
+                        <span class="lock" id="fermerModal">
+                            &times;
+                        </span>
+                        <div class="modal-container-title">
+                            <h4 >
+                               Remerciements pour prise de contact
+                            </h4>
+                        </div>
+                            <hr>
+                        <div class="modal-container-message">
+                            <p >
+                             &nbsp; &nbsp;Cher/Chère ${data.name}, </br>
+                             &nbsp; &nbsp;Je tenais à vous remercier chaleureusement pour avoir pris le temps de me contacter via mon
+                             portfolio. 
+                             Votre message ayant pour objet <b>"${data.subject}"</b> a été reçu avec gratitude.</br>
+                             &nbsp; &nbsp; je vous contacterais via: ${data.email}.</br>
+                             &nbsp; &nbsp; Votre contact est un privilège et je suis impatient de poursuivre nos échanges.</br></br>
+        
+                             &nbsp; &nbsp;
+                             Cordialement,</br>
+                             &nbsp; &nbsp;&nbsp; &nbsp; SIELINOU NOUBISSIE ERIC ROMUALD
+                             
+                            </p>
+                        </div>
+                        <div class="btn-modal">
+                            <div class="btn" id="ok-modal">
+                            ok
+                            </div>
                         </div>
                     </div>
                 </div>
-            </div>
-        `
+            `
+        }
     } else if(langue == 'en') {
         return `
             <div id="maModal" class="modal" >
