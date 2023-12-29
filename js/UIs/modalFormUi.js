11d99e39d678d7f4d56fe25bdbd6d1c478ca7ddf
SIELINOU NOUBISSIE ERIC ROMUALD <sielinou1605@gmail.com>
2023-12-29 14:07:03 +0100

    for pull

--- js/UIs/modalFormUi.js
+++ js/UIs/modalFormUi.js
@@ -3,18 +3,18 @@ function schowModal(data) {
     console.log(langue);
     if(langue == 'fr') {
         return `
-                <div id="maModal" class="modal" >
-                    <div class="modal-container">
-                        <span class="lock" id="fermerModal">
+            <div id="maModal" class="modal" >
+                <div class="modal-container">
+                    <span class="lock" id="fermerModal">
                         &times;
-                        </span>
-                        <div class="modal-container-title">
-                         <h2 >
+                    </span>
+                    <div class="modal-container-title">
+                        <h2 >
                            Remerciements pour prise de contact
-                         </h2>
-                        </div>
+                        </h2>
+                    </div>
                         <hr>
-                        <div class="modal-container-message">
+                    <div class="modal-container-message">
                         <p >
                          &nbsp; &nbsp;Cher/Chère ${data.name}, </br>
                          &nbsp; &nbsp;Je tenais à vous remercier chaleureusement pour avoir pris le temps de me contacter via mon
@@ -28,52 +28,48 @@ function schowModal(data) {
                          &nbsp; &nbsp;&nbsp; &nbsp; SIELINOU NOUBISSIE ERIC ROMUALD
                          
                         </p>
-                        <div class="btn-modal">
+                    </div>
+                    <div class="btn-modal">
                         <div class="btn" id="ok-modal">
-                         ok
-                        </div>
-                        </div>
-                        
+                        ok
                         </div>
                     </div>
                 </div>
+            </div>
         `
     } else if(langue == 'en') {
         return `
-                <div id="maModal" class="modal" >
-                    <div class="modal-container">
-                        <span class="lock" id="fermerModal">
+            <div id="maModal" class="modal" >
+                <div class="modal-container">
+                    <span class="lock" id="fermerModal">
                         &times;
-                        </span>
-                        <div class="modal-container-title">
-                         <h2 >
+                    </span>
+                    <div class="modal-container-title">
+                        <h2 >
                            Thank you for contacting us
-                         </h2>
-                        </div>
+                        </h2>
+                    </div>
                         <hr>
-                        <div class="modal-container-message">
+                    <div class="modal-container-message">
                         <p >
                          &nbsp; &nbsp; Dear ${data.name}, </br>
                          &nbsp; &nbsp; I would like to thank you warmly for taking the time to contact me via my portfolio. 
                          Your message with the subject <b>"${data.subject}"</b> was gratefully received</br>
-                         &nbsp; &nbsp; Your interest in <b>"${data.subject}"</b> is precious to me. I will carefully examine
-                          every detail of your message and undertake to reply as soon as possible using the email address you
-                           havekindly shared: ${data.email}.</br>
+                         &nbsp; &nbsp; I will contact you via : ${data.email}.</br>
                            &nbsp; &nbsp; your contact is a privilege and i look forward to continuing our exchanges.</br></br>
 
                            &nbsp; &nbsp; Sincerely, </br>
                            &nbsp; &nbsp;&nbsp; &nbsp; SIELINOU NOUBISSIE ERIC ROMUALD
                          
                         </p>
-                        <div class="btn-modal">
+                    </div>
+                    <div class="btn-modal">
                         <div class="btn" id="ok-modal">
-                         ok
-                        </div>
-                        </div>
-                        
+                        ok
                         </div>
                     </div>
                 </div>
+            </div>
         `
     }
 }
